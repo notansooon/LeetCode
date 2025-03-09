e95dfb30c880e48336ad7158658c4ade66b6e210
notansooon <ansonjiang043@gmail.com>
2025-03-08 22:12:17 -0500

    update

---
+++ BullsAndCows.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BullsAndCows {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        
+        
+        
+        List<List<Integer>>result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+
+        
+    }
+
+
+    private List<Integer> backTrace(List<Integer> temp, List<List<Integer>> result,int[] candidates, int target) {
+
+    }
+           
+           
+}
--- DeleteNodeInABST.java
+++ DeleteNodeInABST.java
@@ -1,3 +1,49 @@
+// Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+// Basically, the deletion can be divided into two stages:
+// Search for a node to remove.
+// If the node is found, delete the node.
+
+
 public class DeleteNodeInABST {
+    public void deleteNode(TreeNode root, int key) {
+
+
+        if (root == null) return root;
+
+
+        if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        }
+        else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        }
+        else {
+            // No children
+            if (root.left == null && root.right == null) {
+                //return null;
+            }
+
+            
+            else if (root.right == null) {
+                return root.left;
+            }
+            else if (root.left == null) {
+                return root.right;
+            }
+            else {
+                return root.left;
+            }
+
+
+        }
+
+        
+    }
+
+
+
+
+
+
            
 }
