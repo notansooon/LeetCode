807997a6e634c7b56f9b0aa912e4ca9c42a10a74
notansooon <ansonjiang043@gmail.com>
2025-04-01 12:15:31 -0400

    update

--- Reverse_linked_List_II.py
+++ Reverse_linked_List_II.py
@@ -1,5 +1,6 @@
 # Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
 
+# Needs review
 
 class ListNode(object):
     def __init__(self, val=0, next=None):
@@ -7,17 +8,30 @@ class ListNode(object):
         self.next = next
 
 def reverseBetween(self, head, left, right):
-    count = 0
     
-    prev = ListNode(0)
-    prev.next = head
-    curr = prev
     
-    while curr:
+    
+    dummy = ListNode(0)
+    dummy.next = head
+    prev = dummy
+    pos = 1
+    
+    while prev and pos < left:
+        prev = prev.next
+        pox += 1
 
-
-
+    curr = prev.next
+    while curr and pos < right - left:
+        temp = curr.next
+        curr.next = temp.next
+        temp.next = prev.next
+        prev.next = temp
+        pos += 1
 # Input: head = [1,2,3,4,5], left = 2, right = 4
 # Output: [1,4,3,2,5]
+    
+    
+# Input: head = [1,2,3,4,5], left = 2, right = 3
+# Output: [1,3,2,4,5]
     
            
